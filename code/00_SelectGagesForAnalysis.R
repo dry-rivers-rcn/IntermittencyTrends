0118d4b2f4b439a81a7d7e82d036c1ad2c821d6d
Sam Zipper <samuelczipper@gmail.com>
2019-09-26 10:58:07 -0600

    Update gage sample to only western KS for testing

--- code/00_SelectGagesForAnalysis.R
+++ code/00_SelectGagesForAnalysis.R
@@ -10,6 +10,14 @@ gages_annual_summary <-
             "annual_no_flow_and_climate_metric_means_for_no_flow_sites_081919.csv") %>% 
   readr::read_csv()
 
+## locations of stream gages
+sf_gages <- 
+  file.path("data", "USGS_GageLocations.gpkg") %>% 
+  sf::st_read()
+
+# kansas is FIPS 20
+gages_westKS <- subset(sf_gages, state_cd == 20 & dec_long_v < -100)$site
+
 ## variables we care about:
 #  -totalnoflowperwyear = number of days with no flow per water year
 # (variable descriptions: dir_DataAnalysis/code/Variable_explanations.xlsx)
@@ -19,10 +27,14 @@ gages_allyears_summary <-
   gages_annual_summary %>% 
   dplyr::group_by(site) %>% 
   dplyr::summarize(total_flow_years = n(), 
-                   no_flow_years = sum(totalnoflowperwyear >= 1))
+                   no_flow_years = sum(totalnoflowperwyear >= 1)) %>% 
+  dplyr::left_join(sf_gages, by = c("site" = "site_no"))
 
-## subset to gages meeting criteria
-gages_analysis <- gages_allyears_summary[gages_allyears_summary$no_flow_years >= 10, ]
+## subset to gages meeting criteria and in western kansas
+gages_analysis <- gages_allyears_summary[gages_allyears_summary$no_flow_years >= 10 &
+                                           gages_allyears_summary$state_cd == 20 &
+                                           gages_allyears_summary$dec_long_v < -100, ] %>% 
+  na.omit()
 
 # save file
 gages_analysis %>% 
