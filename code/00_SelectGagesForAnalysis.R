bfd010f9a082c14a50dda31a7c1eccb1408ba4c5
Sam Zipper <samuelczipper@gmail.com>
2020-02-10 11:55:50 -0600

    Add tentative region groupings to gage subsample

--- code/00_SelectGagesForAnalysis.R
+++ code/00_SelectGagesForAnalysis.R
@@ -46,12 +46,39 @@ gage_trends <-
   # remove 'currentwyear'
   subset(metric != "currentwyear")
 
+## define regions - for now, use the NA_L1NAME column as a preliminary start and refine it to fewer groups
+gage_sample$region <- NA
+gage_sample$region[gage_sample$NA_L1NAME == "GREAT PLAINS" & gage_sample$dec_lat_va > 43] <- "North Great Plains"
+gage_sample$region[gage_sample$NA_L1NAME == "GREAT PLAINS" & gage_sample$dec_lat_va <= 43] <- "South Great Plains"
+gage_sample$region[gage_sample$NA_L1NAME %in% c("MEDITERRANEAN CALIFORNIA", "MARINE WEST COAST FOREST")] <- "Mediterranean California"
+gage_sample$region[gage_sample$NA_L1NAME %in% c("SOUTHERN SEMI-ARID HIGHLANDS", "NORTH AMERICAN DESERTS")] <- "Western Desert"
+gage_sample$region[gage_sample$NA_L1NAME %in% c("NORTHWESTERN FORESTED MOUNTAINS", "TEMPERATE SIERRAS")] <- "Western Mountains"
+gage_sample$region[gage_sample$NA_L1NAME == "EASTERN TEMPERATE FORESTS"] <- "Eastern Temperate"
+
 ## save data to repository
 gage_sample %>% 
   readr::write_csv(path = file.path("results", "00_SelectGagesForAnalysis_GageSampleMean.csv"))
 
 gage_sample_annual %>% 
+  dplyr::left_join(gage_sample[ , c("gage_ID", "region")], by = "gage_ID") %>% 
   readr::write_csv(path = file.path("results", "00_SelectGagesForAnalysis_GageSampleAnnual.csv"))
 
 gage_trends %>% 
+  dplyr::left_join(gage_sample[ , c("gage_ID", "region")], by = "gage_ID") %>% 
   readr::write_csv(path = file.path("results", "00_SelectGagesForAnalysis_GageSampleTrends.csv"))
+
+## plot
+# load state map
+states <- map_data("state")
+
+ggplot() + 
+  geom_polygon(data = states, aes(x = long, y = lat, group = group), fill = NA, color = col.gray) +
+  geom_point(data = gage_sample, aes(x = dec_long_va, y = dec_lat_va, color = region, shape = CLASS)) +
+  scale_x_continuous(name = "Longitude") +
+  scale_y_continuous(name = "Latitude") +
+  scale_color_discrete(name = "Region") +
+  scale_shape_discrete(name = "Class") +
+  labs(title = "Gages Selected for Analysis", subtitle = "Tentative grouping by regions from NA_L1NAME") +
+  coord_map() +
+  ggsave(file.path("results", "00_SelectGagesForAnalysis_Map.png"),
+         width = 190, height = 90, units= "mm")
