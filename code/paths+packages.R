afed80b20c0691879ec23a6ad7da8452a5d83f5f
Sam Zipper <samuelczipper@gmail.com>
2020-02-10 11:11:13 -0600

    Fix typo

--- code/paths+packages.R
+++ code/paths+packages.R
@@ -13,7 +13,7 @@ dir_DataAnalysis <- file.path("..", "DataAnalysis")
 # OneDrive directory with bigger data files
 dir_data <- file.path("C:/Users/samzipper/OneDrive - The University of Kansas/Research/DryRiversRCN/data")
 
-s## common CRS for projecting files
+## common CRS for projecting files
 proj_crs <- structure(list(epsg = NA_integer_, proj4string = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"), class = "crs")
 
 ## ggplot theme
