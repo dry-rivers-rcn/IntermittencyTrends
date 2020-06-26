6d7aa2bd668a60e652bcadde4384476100237a12
samzipper <samuelczipper@gmail.com>
2020-06-26 17:53:34 -0500

    Remove some highly correlated predictor variables from scripts

--- code/01_RandomForest_PreliminaryVariableImportance.R
+++ code/01_RandomForest_PreliminaryVariableImportance.R
@@ -37,17 +37,14 @@ gage_sample_annual <-
 #   dput(names(gage_sample_annual))
 #   dput(names(gage_sample))
 predictors_climate <- c("p_mm_cy", "p_mm_jas", "p_mm_ond", "p_mm_jfm", "p_mm_amj", "pet_mm_cy", 
-                       "pet_mm_jas", "pet_mm_ond", "pet_mm_jfm", "pet_mm_amj", "T_max_c_cy", 
-                       "T_max_c_jas", "T_max_c_ond", "T_max_c_jfm", "T_max_c_amj", "T_min_c_cy", 
-                       "T_min_c_jas", "T_min_c_ond", "T_min_c_jfm", "T_min_c_amj", "pcumdist10days", 
-                       "pcumdist50days", "pcumdist90days", "swe_mm_cy", "swe_mm_jas", 
-                       "swe_mm_ond", "swe_mm_jfm", "swe_mm_amj", "srad_wm2_cy", "srad_wm2_jas", 
-                       "srad_wm2_ond", "srad_wm2_jfm", "srad_wm2_amj", "pdsi_cy", "pdsi_jas", 
-                       "pdsi_ond", "pdsi_jfm", "pdsi_amj", "p.pet_cy", "swe.p_cy", "p.pet_jfm", "swe.p_jfm",
-                       "p.pet_amj", "swe.p_amj", "p.pet_jas", "swe.p_jas", "p.pet_ond", "swe.p_ond")
+                        "pet_mm_jas", "pet_mm_ond", "pet_mm_jfm", "pet_mm_amj", "T_max_c_cy", 
+                        "T_max_c_jas", "T_max_c_ond", "T_max_c_jfm", "T_max_c_amj",
+                        "swe_mm_cy", "swe_mm_jas", 
+                        "swe_mm_ond", "swe_mm_jfm", "swe_mm_amj", "p.pet_cy", "swe.p_cy", "p.pet_jfm", "swe.p_jfm",
+                        "p.pet_amj", "swe.p_amj", "p.pet_jas", "swe.p_jas", "p.pet_ond", "swe.p_ond")
 
 predictors_human <- c("dams_n", "maxstorage_af", 
-                      "normstorage_af", "majordams_n", "wuse_mm", "irrig_prc", "harvcrop_prc", 
+                      "normstorage_af", "majordams_n", "wuse_mm", "irrig_prc", 
                       "lulc_water_prc", "lulc_dev_prc", "lulc_forest_prc", "lulc_barren_prc", 
                       "lulc_grass_prc", "lulc_ag_prc", "lulc_wetland_prc")
 
@@ -58,21 +55,15 @@ predictors_static <- c("drain_sqkm", "elev_mean_m_basin", "slope_pct",
 # previous year predictors will be calculated further down
 predictors_climate_with_previous <- 
   c(predictors_climate, c("p_mm_cy.previous", "p_mm_jas.previous", "p_mm_ond.previous", 
-                         "p_mm_jfm.previous", "p_mm_amj.previous", "pet_mm_cy.previous", 
-                         "pet_mm_jas.previous", "pet_mm_ond.previous", "pet_mm_jfm.previous", 
-                         "pet_mm_amj.previous", "T_max_c_cy.previous", "T_max_c_jas.previous", 
-                         "T_max_c_ond.previous", "T_max_c_jfm.previous", "T_max_c_amj.previous", 
-                         "T_min_c_cy.previous", "T_min_c_jas.previous", "T_min_c_ond.previous", 
-                         "T_min_c_jfm.previous", "T_min_c_amj.previous", "pcumdist10days.previous", 
-                         "pcumdist50days.previous", "pcumdist90days.previous", "swe_mm_cy.previous", 
-                         "swe_mm_jas.previous", "swe_mm_ond.previous", "swe_mm_jfm.previous", 
-                         "swe_mm_amj.previous", "srad_wm2_cy.previous", "srad_wm2_jas.previous", 
-                         "srad_wm2_ond.previous", "srad_wm2_jfm.previous", "srad_wm2_amj.previous", 
-                         "pdsi_cy.previous", "pdsi_jas.previous", "pdsi_ond.previous", 
-                         "pdsi_jfm.previous", "pdsi_amj.previous", "p.pet_cy.previous", 
-                         "swe.p_cy.previous", "p.pet_jfm.previous", "swe.p_jfm.previous", 
-                         "p.pet_amj.previous", "swe.p_amj.previous", "p.pet_jas.previous", 
-                         "swe.p_jas.previous", "p.pet_ond.previous", "swe.p_ond.previous"))
+                          "p_mm_jfm.previous", "p_mm_amj.previous", "pet_mm_cy.previous", 
+                          "pet_mm_jas.previous", "pet_mm_ond.previous", "pet_mm_jfm.previous", 
+                          "pet_mm_amj.previous", "T_max_c_cy.previous", "T_max_c_jas.previous", 
+                          "T_max_c_ond.previous", "T_max_c_jfm.previous", "T_max_c_amj.previous",
+                          "swe_mm_cy.previous", "swe_mm_jas.previous", "swe_mm_ond.previous", 
+                          "swe_mm_jfm.previous", "swe_mm_amj.previous", "p.pet_cy.previous", 
+                          "swe.p_cy.previous", "p.pet_jfm.previous", "swe.p_jfm.previous", 
+                          "p.pet_amj.previous", "swe.p_amj.previous", "p.pet_jas.previous", 
+                          "swe.p_jas.previous", "p.pet_ond.previous", "swe.p_ond.previous"))
 
 # metrics and regions to predict
 metrics <- c("annualfractionnoflow", "zeroflowfirst", "peak2z_length")
--- code/02_RandomForest_RunModels.R
+++ code/02_RandomForest_RunModels.R
@@ -44,16 +44,13 @@ regions <- c("National", unique(gage_sample$region))
 # all possible predictors
 predictors_climate <- c("p_mm_cy", "p_mm_jas", "p_mm_ond", "p_mm_jfm", "p_mm_amj", "pet_mm_cy", 
                         "pet_mm_jas", "pet_mm_ond", "pet_mm_jfm", "pet_mm_amj", "T_max_c_cy", 
-                        "T_max_c_jas", "T_max_c_ond", "T_max_c_jfm", "T_max_c_amj", "T_min_c_cy", 
-                        "T_min_c_jas", "T_min_c_ond", "T_min_c_jfm", "T_min_c_amj", "pcumdist10days", 
-                        "pcumdist50days", "pcumdist90days", "swe_mm_cy", "swe_mm_jas", 
-                        "swe_mm_ond", "swe_mm_jfm", "swe_mm_amj", "srad_wm2_cy", "srad_wm2_jas", 
-                        "srad_wm2_ond", "srad_wm2_jfm", "srad_wm2_amj", "pdsi_cy", "pdsi_jas", 
-                        "pdsi_ond", "pdsi_jfm", "pdsi_amj", "p.pet_cy", "swe.p_cy", "p.pet_jfm", "swe.p_jfm",
+                        "T_max_c_jas", "T_max_c_ond", "T_max_c_jfm", "T_max_c_amj",
+                        "swe_mm_cy", "swe_mm_jas", 
+                        "swe_mm_ond", "swe_mm_jfm", "swe_mm_amj", "p.pet_cy", "swe.p_cy", "p.pet_jfm", "swe.p_jfm",
                         "p.pet_amj", "swe.p_amj", "p.pet_jas", "swe.p_jas", "p.pet_ond", "swe.p_ond")
 
 predictors_human <- c("dams_n", "maxstorage_af", 
-                      "normstorage_af", "majordams_n", "wuse_mm", "irrig_prc", "harvcrop_prc", 
+                      "normstorage_af", "majordams_n", "wuse_mm", "irrig_prc", 
                       "lulc_water_prc", "lulc_dev_prc", "lulc_forest_prc", "lulc_barren_prc", 
                       "lulc_grass_prc", "lulc_ag_prc", "lulc_wetland_prc")
 
@@ -67,15 +64,9 @@ predictors_climate_with_previous <-
                           "p_mm_jfm.previous", "p_mm_amj.previous", "pet_mm_cy.previous", 
                           "pet_mm_jas.previous", "pet_mm_ond.previous", "pet_mm_jfm.previous", 
                           "pet_mm_amj.previous", "T_max_c_cy.previous", "T_max_c_jas.previous", 
-                          "T_max_c_ond.previous", "T_max_c_jfm.previous", "T_max_c_amj.previous", 
-                          "T_min_c_cy.previous", "T_min_c_jas.previous", "T_min_c_ond.previous", 
-                          "T_min_c_jfm.previous", "T_min_c_amj.previous", "pcumdist10days.previous", 
-                          "pcumdist50days.previous", "pcumdist90days.previous", "swe_mm_cy.previous", 
-                          "swe_mm_jas.previous", "swe_mm_ond.previous", "swe_mm_jfm.previous", 
-                          "swe_mm_amj.previous", "srad_wm2_cy.previous", "srad_wm2_jas.previous", 
-                          "srad_wm2_ond.previous", "srad_wm2_jfm.previous", "srad_wm2_amj.previous", 
-                          "pdsi_cy.previous", "pdsi_jas.previous", "pdsi_ond.previous", 
-                          "pdsi_jfm.previous", "pdsi_amj.previous", "p.pet_cy.previous", 
+                          "T_max_c_ond.previous", "T_max_c_jfm.previous", "T_max_c_amj.previous",
+                          "swe_mm_cy.previous", "swe_mm_jas.previous", "swe_mm_ond.previous", 
+                          "swe_mm_jfm.previous", "swe_mm_amj.previous", "p.pet_cy.previous", 
                           "swe.p_cy.previous", "p.pet_jfm.previous", "swe.p_jfm.previous", 
                           "p.pet_amj.previous", "swe.p_amj.previous", "p.pet_jas.previous", 
                           "swe.p_jas.previous", "p.pet_ond.previous", "swe.p_ond.previous"))
