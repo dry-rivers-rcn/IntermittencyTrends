1bbfa8ab22ed0400908678dac6f0b3d26a1d9f91
samzipper <samuelczipper@gmail.com>
2020-07-09 15:37:32 -0500

    Experimenting with tidymodels approach to random forest construction

--- code/RandomForest_MessingAround.R
+++ code/RandomForest_MessingAround.R
@@ -26,76 +26,39 @@ gage_sample_annual <-
                 p.pet_ond = p_mm_ond/pet_mm_ond,
                 swe.p_ond = swe_mm_ond/p_mm_ond)
 
-rf_var_importance <- 
-  file.path("results", "01_RandomForest_PreliminaryVariableImportance.csv") %>% 
-  readr::read_csv() %>% 
-  dplyr::group_by(metric, region, predictor) %>% 
-  dplyr::summarize(PrcIncMSE_mean = mean(PrcIncMSE)) %>% 
-  dplyr::ungroup()
-
 ## set up predictions
 # metrics and regions to predict
 metrics <- c("annualfractionnoflow", "zeroflowfirst", "peak2z_length")
 regions <- c("National", unique(gage_sample$region))
 
 # all possible predictors
-predictors_annual <- c("p_mm_cy", "p_mm_jas", "p_mm_ond", "p_mm_jfm", "p_mm_amj", "pet_mm_cy", 
-                       "pet_mm_jas", "pet_mm_ond", "pet_mm_jfm", "pet_mm_amj", "T_max_c_cy", 
-                       "T_max_c_jas", "T_max_c_ond", "T_max_c_jfm", "T_max_c_amj", "T_min_c_cy", 
-                       "T_min_c_jas", "T_min_c_ond", "T_min_c_jfm", "T_min_c_amj", "pcumdist10days", 
-                       "pcumdist50days", "pcumdist90days", "swe_mm_cy", "swe_mm_jas", 
-                       "swe_mm_ond", "swe_mm_jfm", "swe_mm_amj", "srad_wm2_cy", "srad_wm2_jas", 
-                       "srad_wm2_ond", "srad_wm2_jfm", "srad_wm2_amj", "pdsi_cy", "pdsi_jas", 
-                       "pdsi_ond", "pdsi_jfm", "pdsi_amj", "p.pet_cy", "swe.p_cy", "p.pet_jfm", "swe.p_jfm",
-                       "p.pet_amj", "swe.p_amj", "p.pet_jas", "swe.p_jas", "p.pet_ond", "swe.p_ond")
+predictors_climate <- c("p_mm_cy", "p_mm_jas", "p_mm_ond", "p_mm_jfm", "p_mm_amj", "pet_mm_cy", 
+                        "pet_mm_jas", "pet_mm_ond", "pet_mm_jfm", "pet_mm_amj", "T_max_c_cy", 
+                        "T_max_c_jas", "T_max_c_ond", "T_max_c_jfm", "T_max_c_amj",
+                        "swe_mm_cy", "swe_mm_jas", 
+                        "swe_mm_ond", "swe_mm_jfm", "swe_mm_amj", "p.pet_cy", "swe.p_cy", "p.pet_jfm", "swe.p_jfm",
+                        "p.pet_amj", "swe.p_amj", "p.pet_jas", "swe.p_jas", "p.pet_ond", "swe.p_ond")
+
+predictors_human <- c("dams_n", "maxstorage_af", "normstorage_af", "majordams_n", 
+                      "wuse_mm", "irrig_prc", "lulc_water_prc", "lulc_dev_prc", "lulc_wetland_prc",
+                      "lulc_forest_prc", "lulc_barren_prc", "lulc_grass_prc", "lulc_ag_prc")
 
 predictors_static <- c("drain_sqkm", "elev_mean_m_basin", "slope_pct", 
                        "awcave", "permave", "topwet", "depth_bedrock_m", 
                        "porosity", "storage_m", "clayave", "siltave", "sandave")
 
 # previous year predictors will be calculated further down
-predictors_annual_with_previous <- 
-  c(predictors_annual, c("p_mm_cy.previous", "p_mm_jas.previous", "p_mm_ond.previous", 
-                         "p_mm_jfm.previous", "p_mm_amj.previous", "pet_mm_cy.previous", 
-                         "pet_mm_jas.previous", "pet_mm_ond.previous", "pet_mm_jfm.previous", 
-                         "pet_mm_amj.previous", "T_max_c_cy.previous", "T_max_c_jas.previous", 
-                         "T_max_c_ond.previous", "T_max_c_jfm.previous", "T_max_c_amj.previous", 
-                         "T_min_c_cy.previous", "T_min_c_jas.previous", "T_min_c_ond.previous", 
-                         "T_min_c_jfm.previous", "T_min_c_amj.previous", "pcumdist10days.previous", 
-                         "pcumdist50days.previous", "pcumdist90days.previous", "swe_mm_cy.previous", 
-                         "swe_mm_jas.previous", "swe_mm_ond.previous", "swe_mm_jfm.previous", 
-                         "swe_mm_amj.previous", "srad_wm2_cy.previous", "srad_wm2_jas.previous", 
-                         "srad_wm2_ond.previous", "srad_wm2_jfm.previous", "srad_wm2_amj.previous", 
-                         "pdsi_cy.previous", "pdsi_jas.previous", "pdsi_ond.previous", 
-                         "pdsi_jfm.previous", "pdsi_amj.previous", "p.pet_cy.previous", 
-                         "swe.p_cy.previous", "p.pet_jfm.previous", "swe.p_jfm.previous", 
-                         "p.pet_amj.previous", "swe.p_amj.previous", "p.pet_jas.previous", 
-                         "swe.p_jas.previous", "p.pet_ond.previous", "swe.p_ond.previous"))
-
-## divide gage sample into train (80% of ref), test (20% of ref), and non-ref (all non-ref)
-# choose fraction of gages to use as validation
-frac_val <- 0.8
-
-# want to use same sample for all regions, metrics so need to take 80% from each region
-set.seed(1)
-gage_val_sample <-
-  gage_sample %>% 
-  subset(CLASS == "Ref") %>% 
-  dplyr::group_by(region) %>% 
-  dplyr::sample_frac(frac_val) %>% 
-  dplyr::ungroup() %>% 
-  dplyr::select(gage_ID) %>% 
-  dplyr::mutate(Sample = "Train")
-
-# add to gage_sample; everything that is not selected will be Train (if ref) or non-ref
-gage_sample <- dplyr::left_join(gage_sample, gage_val_sample, by = "gage_ID")
-gage_sample$Sample[is.na(gage_sample$Sample) & gage_sample$CLASS == "Non-ref"] <- "Non-ref"
-gage_sample$Sample[is.na(gage_sample$Sample) & gage_sample$CLASS == "Ref"] <- "Test"
-
-# check count in each sample
-gage_sample %>% 
-  dplyr::select(region, Sample) %>% 
-  table()
+predictors_climate_with_previous <- 
+  c(predictors_climate, c("p_mm_cy.previous", "p_mm_jas.previous", "p_mm_ond.previous", 
+                          "p_mm_jfm.previous", "p_mm_amj.previous", "pet_mm_cy.previous", 
+                          "pet_mm_jas.previous", "pet_mm_ond.previous", "pet_mm_jfm.previous", 
+                          "pet_mm_amj.previous", "T_max_c_cy.previous", "T_max_c_jas.previous", 
+                          "T_max_c_ond.previous", "T_max_c_jfm.previous", "T_max_c_amj.previous",
+                          "swe_mm_cy.previous", "swe_mm_jas.previous", "swe_mm_ond.previous", 
+                          "swe_mm_jfm.previous", "swe_mm_amj.previous", "p.pet_cy.previous", 
+                          "swe.p_cy.previous", "p.pet_jfm.previous", "swe.p_jfm.previous", 
+                          "p.pet_amj.previous", "swe.p_amj.previous", "p.pet_jas.previous", 
+                          "swe.p_jas.previous", "p.pet_ond.previous", "swe.p_ond.previous"))
 
 ## calculate previous water year climate metrics
 gage_sample_prevyear <- 
@@ -107,17 +70,18 @@ gage_sample_prevyear <-
 fit_data_in <- 
   gage_sample_annual %>% 
   # subset to fewer columns - metrics and predictors
-  dplyr::select(c("gage_ID", "currentclimyear", all_of(metrics), all_of(predictors_annual))) %>% 
+  dplyr::select(c("gage_ID", "currentclimyear", all_of(metrics), 
+                  all_of(predictors_climate), all_of(predictors_human))) %>% 
   # join with previous water year
   dplyr::left_join(gage_sample_prevyear, 
                    by = c("gage_ID", "currentclimyear"="wyearjoin"), 
                    suffix = c("", ".previous")) %>% 
   # join with static predictors
-  dplyr::left_join(gage_sample[ , c("gage_ID", "CLASS", "region", "Sample", predictors_static)], by = "gage_ID")
+  dplyr::left_join(gage_sample[ , c("gage_ID", "CLASS", "Sample", "region", predictors_static)], by = "gage_ID")
 
 ## set metric and run model
-metrics <- c("annualfractionnoflow", "zeroflowfirst", "peak2z_length")
-m <- "annualfractionnoflow"
+m <- "peak2z_length"
+r <- "National"
 predictors <- c(predictors_annual, predictors_annual_with_previous, predictors_static)
 
 # get rid of unneeded metrics
@@ -127,6 +91,146 @@ fit_data_m <-
 
 # rename metric column
 names(fit_data_m)[names(fit_data_m) == m] <- "observed"
+
+# get predictor variables
+n_pred <- 18
+rf_var_m_r <-
+  file.path("results", paste0("01_RandomForest_PreliminaryVariableImportance_", m, "_", gsub(" ", "", r, fixed = TRUE), ".csv")) %>% 
+  readr::read_csv() %>% 
+  dplyr::group_by(metric, region, predictor) %>% 
+  dplyr::summarize(PrcIncMSE_mean = mean(PrcIncMSE)) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::top_n(n = n_pred, wt = PrcIncMSE_mean)
+
+if (r == "National") {
+  fit_data_r <- 
+    fit_data_m %>% 
+    dplyr::select(gage_ID, CLASS, Sample, currentclimyear, observed, region, all_of(rf_var_m_r$predictor)) %>% 
+    subset(complete.cases(.))
+} else {
+  fit_data_r <- 
+    fit_data_m %>% 
+    subset(region == r) %>% 
+    dplyr::select(gage_ID, CLASS, Sample, currentclimyear, observed, region, all_of(rf_var_m_r$predictor)) %>% 
+    subset(complete.cases(.))
+}
+
+fit_formula <- as.formula(paste0("observed ~ ", paste(unique(rf_var_m_r$predictor), collapse = "+")))
+
+## k-folds cross-validation and prediction
+set.seed(1)
+n_folds <- length(unique(gage_sample$Sample)) # choose number of folds for cross-val
+for (k in 1:n_folds){
+  ## train model
+  fit_rf <- randomForest::randomForest(fit_formula,
+                                       data = subset(fit_data_r, Sample != paste0("Test", k)),
+                                       ntree = 500,
+                                       localImp = T)
+  
+  # run model
+  fit_data_r$predicted <- predict(fit_rf, fit_data_r)
+  fit_data_r$metric <- m
+  fit_data_r$region_rf <- r
+  fit_data_i <- 
+    fit_data_r %>% 
+    dplyr::select(gage_ID, CLASS, currentclimyear, observed, predicted, metric, region_rf) %>% 
+    dplyr::mutate(kfold = k)
+  
+  # extract variable importance
+  fit_rf_imp_i <- tibble::tibble(predictor = rownames(randomForest::importance(fit_rf, type = 1)),
+                                 VarPrcIncMSE = randomForest::importance(fit_rf, type = 1)[,'%IncMSE'],
+                                 metric = m,
+                                 region_rf = r,
+                                 kfold = k)
+  
+  if (k == 1){
+    fit_data_out <- fit_data_i
+    fit_rf_imp <- fit_rf_imp_i
+  } else {
+    fit_data_out <- dplyr::bind_rows(fit_data_out, fit_data_i)
+    fit_rf_imp <- dplyr::bind_rows(fit_rf_imp, fit_rf_imp_i)
+  }
+  
+  # status update
+  print(paste0(m, " ", r, " k", k, " complete, ", Sys.time()))
+  
+}
+
+# identify test vs. train
+rf_all <- 
+  fit_data_out %>% 
+  dplyr::mutate(residual = predicted - observed) %>% 
+  dplyr::left_join(gage_sample[,c("gage_ID", "region", "Sample")], by = "gage_ID") %>% 
+  dplyr::mutate(Test = Sample == paste0("Test", kfold))
+
+# plot
+rf_all %>% 
+  ggplot(aes(x = predicted, y = observed)) + 
+  geom_point(shape = 1, aes(color = region)) +
+  geom_abline(intercept = 0, slope = 1) +
+  facet_grid(region ~ Test) +
+  scale_color_manual(name = "Region", values = pal_regions) +
+  scale_x_continuous(name = "Predicted [days]",
+                     limits = c(0,225), expand = c(0,0)) +
+  scale_y_continuous(name = "Observed [days]",
+                     limits = c(0,225), expand = c(0,0)) +
+  theme(legend.position = "bottom") +
+  NULL
+
+# fit stats
+rf_fit <-
+  rf_all %>% 
+  dplyr::group_by(Test, region) %>% 
+  dplyr::summarize(RMSE = round(hydroGOF::rmse(predicted, observed), 3),
+                   NRMSE = hydroGOF::nrmse(predicted, observed, norm = "maxmin"),
+                   KGE = round(hydroGOF::KGE(predicted, observed, method = "2012"), 3)) %>% 
+  dplyr::ungroup()
+
+# test mtry and ntree
+mtry_all <- seq(3, 10)
+ntree_all <- seq(100, 1000, 100)
+for (m in mtry_all){
+  for (n in ntree_all){
+    k <- 1
+    fit_data_r$Test <-  fit_data_r$Sample == paste0("Test", k)
+    fit_rf <- randomForest::randomForest(fit_formula,
+                                         data = subset(fit_data_r, Test),
+                                         mtry = m,
+                                         ntree = n,
+                                         localImp = T)
+    
+    fit_data_r$predicted <- predict(fit_rf, fit_data_r)
+    
+    # calculate fit
+    fit_tune_m <- 
+      fit_data_r %>% 
+      dplyr::group_by(Test) %>% 
+      dplyr::summarize(MSE = round(hydroGOF::mse(predicted, observed), 3)) %>% 
+      dplyr::mutate(mtry = m, 
+                    ntree = n)
+    
+    if (m == mtry_all[1] & n == ntree_all[1]){
+      fit_tune <- fit_tune_m
+    } else {
+      fit_tune <- dplyr::bind_rows(fit_tune, fit_tune_m)
+    }
+    print(paste0("mtry ", m, ", ntree ", n, " complete"))
+    
+  }
+}
+
+fit_tune %>% 
+  subset(Test) %>% 
+  ggplot(aes(x = mtry, y = ntree, fill = MSE)) +
+  geom_raster()
+
+
+
+
+
+
+
+
 
 # subset to complete cases only
 fit_data_ref <- 
---
+++ code/RandomForest_TuneModels.R
@@ -0,0 +1,184 @@
+## RandomForest_TuneModels.R
+#' This script is intended to tune random forest models by:
+#'   - determining the most important variables
+#'     - this study compared approaches: https://academic.oup.com/bib/article/20/2/492/4554516
+#'   - determining the best mtry and ntree
+
+source(file.path("code", "paths+packages.R"))
+library(tidymodels)
+library(ranger)
+
+####
+#### prep data
+####
+
+## load data - gage mean properties and annual values
+gage_sample <- 
+  readr::read_csv(file = file.path("results", "00_SelectGagesForAnalysis_GageSampleMean.csv")) %>% 
+  dplyr::mutate(gage_ID = as.numeric(gage_ID))
+
+gage_regions <- 
+  readr::read_csv(file.path("results", "00_SelectGagesForAnalysis_GageRegions.csv"))
+
+gage_sample_annual <-
+  readr::read_csv(file = file.path("results", "00_SelectGagesForAnalysis_GageSampleAnnual.csv")) %>% 
+  dplyr::left_join(gage_regions, by = "gage_ID") %>% 
+  # add some derived variables
+  dplyr::mutate(p.pet_cy = p_mm_cy/pet_mm_cy,
+                swe.p_cy = swe_mm_cy/p_mm_cy,
+                p.pet_jfm = p_mm_jfm/pet_mm_jfm,
+                swe.p_jfm = swe_mm_jfm/p_mm_jfm,
+                p.pet_amj = p_mm_amj/pet_mm_amj,
+                swe.p_amj = swe_mm_amj/p_mm_amj,
+                p.pet_jas = p_mm_jas/pet_mm_jas,
+                swe.p_jas = swe_mm_jas/p_mm_jas,
+                p.pet_ond = p_mm_ond/pet_mm_ond,
+                swe.p_ond = swe_mm_ond/p_mm_ond)
+
+## set up predictions
+# metrics and regions to predict
+metrics <- c("annualfractionnoflow", "zeroflowfirst", "peak2z_length")
+regions <- c("National", unique(gage_sample$region))
+
+# all possible predictors
+predictors_climate <- c("p_mm_cy", "p_mm_jas", "p_mm_ond", "p_mm_jfm", "p_mm_amj", "pet_mm_cy", 
+                        "pet_mm_jas", "pet_mm_ond", "pet_mm_jfm", "pet_mm_amj", "T_max_c_cy", 
+                        "T_max_c_jas", "T_max_c_ond", "T_max_c_jfm", "T_max_c_amj",
+                        "swe_mm_cy", "swe_mm_jas", 
+                        "swe_mm_ond", "swe_mm_jfm", "swe_mm_amj", "p.pet_cy", "swe.p_cy", "p.pet_jfm", "swe.p_jfm",
+                        "p.pet_amj", "swe.p_amj", "p.pet_jas", "swe.p_jas", "p.pet_ond", "swe.p_ond")
+
+predictors_human <- c("dams_n", "maxstorage_af", "normstorage_af", "majordams_n", 
+                      "wuse_mm", "irrig_prc", "lulc_water_prc", "lulc_dev_prc", "lulc_wetland_prc",
+                      "lulc_forest_prc", "lulc_barren_prc", "lulc_grass_prc", "lulc_ag_prc")
+
+predictors_static <- c("drain_sqkm", "elev_mean_m_basin", "slope_pct", 
+                       "awcave", "permave", "topwet", "depth_bedrock_m", 
+                       "porosity", "storage_m", "clayave", "siltave", "sandave")
+
+# previous year predictors will be calculated further down
+predictors_climate_with_previous <- 
+  c(predictors_climate, c("p_mm_cy.previous", "p_mm_jas.previous", "p_mm_ond.previous", 
+                          "p_mm_jfm.previous", "p_mm_amj.previous", "pet_mm_cy.previous", 
+                          "pet_mm_jas.previous", "pet_mm_ond.previous", "pet_mm_jfm.previous", 
+                          "pet_mm_amj.previous", "T_max_c_cy.previous", "T_max_c_jas.previous", 
+                          "T_max_c_ond.previous", "T_max_c_jfm.previous", "T_max_c_amj.previous",
+                          "swe_mm_cy.previous", "swe_mm_jas.previous", "swe_mm_ond.previous", 
+                          "swe_mm_jfm.previous", "swe_mm_amj.previous", "p.pet_cy.previous", 
+                          "swe.p_cy.previous", "p.pet_jfm.previous", "swe.p_jfm.previous", 
+                          "p.pet_amj.previous", "swe.p_amj.previous", "p.pet_jas.previous", 
+                          "swe.p_jas.previous", "p.pet_ond.previous", "swe.p_ond.previous"))
+
+predictors_all <- c(predictors_climate, predictors_human, predictors_static, predictors_climate_with_previous)
+
+## calculate previous water year climate metrics
+gage_sample_prevyear <- 
+  gage_sample_annual[,c("gage_ID", "currentclimyear", predictors_climate)] %>% 
+  dplyr::mutate(wyearjoin = currentclimyear + 1) %>% 
+  dplyr::select(-currentclimyear)
+
+## combine into one data frame
+fit_data_in <- 
+  gage_sample_annual %>% 
+  # subset to fewer columns - metrics and predictors
+  dplyr::select(c("gage_ID", "currentclimyear", all_of(metrics), 
+                  all_of(predictors_climate), all_of(predictors_human))) %>% 
+  # join with previous water year
+  dplyr::left_join(gage_sample_prevyear, 
+                   by = c("gage_ID", "currentclimyear"="wyearjoin"), 
+                   suffix = c("", ".previous")) %>% 
+  # join with static predictors
+  dplyr::left_join(gage_sample[ , c("gage_ID", "CLASS", "Sample", "region", predictors_static)], by = "gage_ID")
+
+## reduce to a subset of data for developing approach
+fit_data_play <- 
+  fit_data_in %>% 
+  dplyr::sample_frac(0.1)
+
+####
+#### build model with tidymodels framework
+####
+
+m <- "annualfractionnoflow"
+r <- "National"
+predictors <- c(predictors_annual, predictors_annual_with_previous, predictors_static)
+
+# get rid of unneeded metrics
+fit_data_m <- 
+  fit_data_play %>% 
+  dplyr::select(-all_of(metrics[metrics != m]))
+
+# rename metric column
+names(fit_data_m)[names(fit_data_m) == m] <- "observed"
+
+# subset to region
+fit_data_r <- 
+  fit_data_m %>% 
+  subset(complete.cases(.))
+
+# split into training/testing
+rf_split <- initial_split(fit_data_r, prop = 0.8)
+
+# set up recipe (formula)
+fit_formula <- as.formula(paste0("observed ~ ", paste(predictors_all, collapse = "+")))
+rf_recipe <- 
+  training(rf_split) %>% 
+  recipe(fit_formula) %>% 
+  step_corr(all_predictors()) %>%
+  step_center(all_predictors(), -all_outcomes()) %>%
+  step_scale(all_predictors(), -all_outcomes()) %>%
+  prep()
+
+# transform test data
+rf_testing <-
+  rf_recipe %>% 
+  bake(testing(rf_split))
+
+# prep training data
+rf_training <- juice(rf_recipe)
+
+# build model
+rf_ranger <- 
+  rand_forest(trees = 100, mode = "regression") %>% 
+  set_engine("ranger") %>% 
+  fit(observed ~ ., data = rf_training)
+
+# check fit
+rf_ranger %>%
+  predict(rf_testing) %>%
+  bind_cols(rf_testing) %>%
+  metrics(truth = observed, estimate = .pred)
+
+
+
+
+
+fit_formula <- as.formula(paste0("observed ~ ", paste(predictors_all, collapse = "+")))
+system.time(fit_rf <- randomForest::randomForest(fit_formula,
+                                                 data = fit_data_r,
+                                                 ntree = 500))
+system.time(fit_rg <- ranger::ranger(fit_formula,
+                                     data = fit_data_r,
+                                     num.trees = 500,
+                                     importance = "permutation",
+                                     seed = 1))
+fit_rg
+fit_rf
+
+ranger::importance(fit_rg)
+
+fit_ct <-
+  train(fit_formula,
+        data = fit_data_r,
+        method = "ranger",
+        trControl = trainControl(method="cv", number = 5, verboseIter = T),
+        tuneGrid = expand.grid(
+          mtry = 2:4,
+          splitrule = "gini",
+          min.node.size),
+        num.trees = 100,
+        importance = "permutation")
+varImp(fit_ct)
+
+Boruta::Boruta(fit_formula, data = fit_data_r, holdHistory = F) -> BorutaTest
+Boruta::Boruta(fit_formula, data = fit_data_r, getImp = getImpFerns, holdHistory = F) -> BorutaTestFerns
