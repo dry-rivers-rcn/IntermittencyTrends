f32d1065f2dd3d76f833f37162d644cc0053eff5
samzipper <samuelczipper@gmail.com>
2020-08-19 09:32:28 -0500

    Working on Mann-Whitney figure

---
+++ code/Trends_ExploreResults.R
@@ -0,0 +1,162 @@
+## Trends_ExploreResults.R
+#' This script load the trend results and calculates some simple 
+#' summary statistics for the trends.
+#' 
+
+source(file.path("code", "paths+packages.R"))
+
+## metrics we care about
+metrics <- c("annualflowdays", "zeroflowfirst", "peak2z_length")
+
+## load data
+gage_regions <- 
+  readr::read_csv(file.path("results", "00_SelectGagesForAnalysis_GageRegions.csv"))
+
+gage_mean <- 
+  readr::read_csv(file.path("results", "00_SelectGagesForAnalysis_GageSampleMean.csv"))
+
+gage_trends <- 
+  readr::read_csv(file.path("results", "00_SelectGagesForAnalysis_GageSampleTrends.csv")) %>% 
+  subset(metric %in% metrics) %>% 
+  dplyr::left_join(gage_regions, by = "gage_ID") %>% 
+  dplyr::left_join(gage_mean[,c("gage_ID", "dec_lat_va", "dec_long_va")], by = "gage_ID")
+
+## plot: mann-kendall results
+# load state map
+states <- map_data("state")
+
+df_mk <- 
+  gage_trends %>% 
+  dplyr::select(metric, gage_ID, region, dec_lat_va, dec_long_va, mk_tau, mk_p) %>% 
+  subset(complete.cases(.))
+
+ggplot() +
+  geom_polygon(data = states, aes(x = long, y = lat, group = group),
+               fill = "gray75", color = "white") +
+  geom_point(data = df_mk,  aes(x = dec_long_va, y = dec_lat_va,
+                                color = mk_tau, shape = mk_p < p_thres)) +
+  facet_wrap(~metric, labeller = 
+               as_labeller(c("annualflowdays" = "(a) Annual Days with Flow",
+                             "peak2z_length" = "(b) Days from Peak to No-Flow",
+                             "zeroflowfirst" = "(c) First No-Flow Day"))) +
+  scale_x_continuous(name = "Longitude [\u00B0E]") +
+  scale_y_continuous(name = "Latitude [\u00B0N]",
+                     breaks = seq(30, 50, 10)) +
+  scale_color_gradient2(name = "Kendall \u03c4",
+                        high = "#4575b4", mid = "#ffffbf", low = "#d73027") +
+  scale_shape_manual(name = "Significance", values = c("TRUE" = 16, "FALSE" = 1),
+                     labels = c("TRUE" = "p < 0.05", "FALSE" = "p > 0.05"))  +
+  theme(panel.border = element_blank(),
+        strip.text = element_text(face = "bold"),
+        legend.position = "bottom") +
+  guides(color = guide_colorbar(order = 1, title.position = "top", title.hjust = 0.5),
+         shape = guide_legend(order = 2, direction = "vertical"))
+
+## plot: mann-whitney results
+df_mw <- 
+  gage_trends %>% 
+  dplyr::mutate(mw_diff_mean = mw_meanGroup2 - mw_meanGroup1,
+                mw_diff_median = mw_medianGroup2 - mw_medianGroup1) %>% 
+  dplyr::select(metric, gage_ID, region, dec_lat_va, dec_long_va, mw_p, mw_diff_mean, mw_diff_median) %>% 
+  subset(complete.cases(.))
+
+# histograms
+ggplot() +
+  geom_histogram(data = df_mw, aes(x = mw_diff_mean, fill = mw_p < p_thres),
+                 binwidth = 10) +
+  geom_vline(xintercept = 0, color = "black") +
+  facet_wrap(~metric, scales = "free", labeller = 
+               as_labeller(c("annualflowdays" = "(a) Annual Days with Flow",
+                             "peak2z_length" = "(b) Days from Peak to No-Flow",
+                             "zeroflowfirst" = "(c) First No-Flow Day"))) +
+  scale_x_continuous(name = "Change in Annual Value [1999 to 2017 Mean - 1980 to 1998 Mean]") +
+  scale_y_continuous(name = "Number of Gages") +
+  scale_fill_manual(name = "Mann-Whitney Significance", values = c("TRUE" = col.cat.blu, "FALSE" = col.gray),
+                    labels = c("TRUE" = "p < 0.05", "FALSE" = "p > 0.05"))  +
+  theme(panel.border = element_blank(),
+        strip.text = element_text(face = "bold"),
+        legend.position = "bottom") +
+  guides(color = guide_colorbar(order = 1, title.position = "top", title.hjust = 0.5),
+         shape = guide_legend(order = 2, direction = "vertical"))
+
+# maps
+ggplot() +
+  geom_polygon(data = states, aes(x = long, y = lat, group = group),
+               fill = "gray75", color = "white") +
+  geom_point(data = df_mw,  aes(x = dec_long_va, y = dec_lat_va,
+                                color = mw_diff, shape = mw_p < p_thres)) +
+  facet_wrap(~metric, labeller = 
+               as_labeller(c("annualflowdays" = "(a) Annual Days with Flow",
+                             "peak2z_length" = "(b) Days from Peak to No-Flow",
+                             "zeroflowfirst" = "(c) First No-Flow Day"))) +
+  scale_x_continuous(name = "Longitude [\u00B0E]") +
+  scale_y_continuous(name = "Latitude [\u00B0N]",
+                     breaks = seq(30, 50, 10)) +
+  scale_color_gradient2(name = "Change in Annual Mean Value",
+                        high = "#4575b4", mid = "#ffffbf", low = "#d73027") +
+  scale_shape_manual(name = "Significance", values = c("TRUE" = 16, "FALSE" = 1),
+                     labels = c("TRUE" = "p < 0.05", "FALSE" = "p > 0.05"))  +
+  theme(panel.border = element_blank(),
+        strip.text = element_text(face = "bold"),
+        legend.position = "bottom") +
+  guides(color = guide_colorbar(order = 1, title.position = "top", title.hjust = 0.5),
+         shape = guide_legend(order = 2, direction = "vertical"))
+
+
+## summarize
+p_thres <- 0.05
+gage_trends %>% 
+  dplyr::group_by(metric) %>% 
+  dplyr::summarize(n_finite = sum(is.finite(slope)),
+                   n_sig = sum(pval < p_thres, na.rm = T),
+                   n_sig_pos = sum(slope > 0 & pval < p_thres, na.rm = T),
+                   n_sig_neg = sum(slope <= 0 & pval < p_thres, na.rm = T))
+
+207/512
+34/150
+23/123
+
+
+
+## reproducible example - exploring mann-kendal and sen's slope
+# sample data
+df <- 
+  tibble::tibble(year = seq(1980, 2017),
+                 value = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+                           0, 0, 0, 0, 0, 0, 0, 0, 0, 0.071232877, 0.082191781, 0.008219178, 
+                           0, 0.008219178, 0, 0.273972603, 0.270491803, 0.01369863, 0, 0, 
+                           0.092896175, 0.090410959, 0, 0, 0, 0, 0.024657534))
+
+
+# make sample data with linear increase
+year <- seq(1, 51)
+value <- seq(0, 500, 10)
+
+# replace >50% of values with 0s
+value[seq(1,51,2)] <- 0
+
+# plot
+plot(year, value)
+
+# calculate mann-kendall tau and theil-sen slope
+manken <- rkt::rkt(year, value)
+manken$tau  # Kendall tau = 0.235
+manken$sl   # p-value = 0.009
+manken$B    # Theil-Sen estimator = 0
+
+# test linear slope
+linfit <- lm(value ~ year)
+summary(linfit)
+
+## now, set >50% of values to zero
+df$value_with_0s <- df$value
+df$value_with_0s[seq(1,51,2)] <- 0
+
+# plot
+plot(df$year, df$value_with_0s)
+
+# calculate mann-kendall tau and theil-sen slope
+manken_with_0s <- rkt::rkt(df$year, df$value_with_0s)
+manken_with_0s$tau  # Kendall tau = 0.23
+manken_with_0s$sl   # p-value = 0.009 (significant)
+manken_with_0s$B    # Theil-Sen slope = 0 becuase > 50% of points are the same
