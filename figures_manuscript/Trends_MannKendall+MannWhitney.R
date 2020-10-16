e3077ef9332f101b7b35dd0b019aa2b0b6764e55
samzipper <samuelczipper@gmail.com>
2020-10-16 14:19:19 -0500

    Edits to Mann-Whitney figure

--- figures_manuscript/Trends_MannKendall+MannWhitney.R
+++ figures_manuscript/Trends_MannKendall+MannWhitney.R
@@ -154,53 +154,169 @@ df_mw <-
   dplyr::select(metric, gage_ID, region, dec_lat_va, dec_long_va, mw_p, mw_diff_mean, mw_diff_median) %>% 
   subset(complete.cases(.))
 
+# make a column for Mann-Whitney about significance and directio of change
+df_mw$mw_sig[df_mw$mw_p > p_thres] <- "NotSig"
+df_mw$mw_sig[df_mw$mw_p < p_thres & 
+               df_mw$mw_diff_mean < 0 & 
+               df_mw$metric %in% c("zeroflowfirst", "peak2z_length")] <- "SigDry"
+df_mw$mw_sig[df_mw$mw_p < p_thres & 
+               df_mw$mw_diff_mean < 0 & 
+               df_mw$metric %in% c("annualnoflowdays")] <- "SigWet"
+df_mw$mw_sig[df_mw$mw_p < p_thres & 
+               df_mw$mw_diff_mean > 0 & 
+               df_mw$metric %in% c("zeroflowfirst", "peak2z_length")] <- "SigWet"
+df_mw$mw_sig[df_mw$mw_p < p_thres & 
+               df_mw$mw_diff_mean > 0 & 
+               df_mw$metric %in% c("annualnoflowdays")] <- "SigDry"
+
 # histograms
 p_mw_hist <-
   ggplot() +
-  geom_histogram(data = df_mw, aes(x = mw_diff_mean, fill = mw_p < p_thres),
+  geom_histogram(data = df_mw, aes(x = mw_diff_mean, fill = mw_sig),
                  binwidth = 10) +
   geom_vline(xintercept = 0, color = "black") +
-  facet_wrap(~metric, ncol = 1, scales = "free", labeller = 
-               as_labeller(c("annualflowdays" = "(a) Annual Days with Flow",
-                             "peak2z_length" = "(c) Days from Peak to No-Flow",
-                             "zeroflowfirst" = "(e) First No-Flow Day"))) +
-  scale_x_continuous(name = "Change in Annual Mean\n[(1999 to 2017) - (1980 to 1998)]") +
+  facet_wrap(~metric, ncol = 3, scales = "free", labeller = 
+               as_labeller(c("annualnoflowdays" = "(a) Annual No-Flow Days",
+                             "peak2z_length" = "(b) Days from Peak to No-Flow",
+                             "zeroflowfirst" = "(c) First No-Flow Day"))) +
+  scale_x_continuous(name = "Change in Annual Mean [(1999 to 2017) - (1980 to 1998)]") +
   scale_y_continuous(name = "Number of Gages") +
-  scale_fill_manual(name = "Mann-Whitney Significance", values = c("TRUE" = col.cat.grn, "FALSE" = col.gray),
-                    labels = c("TRUE" = "p < 0.05", "FALSE" = "p > 0.05"))  +
+  scale_fill_manual(name = "Mann-Whitney Significance", 
+                    values = c("SigDry" = col.cat.red, "SigWet" = col.cat.blu, "NotSig" = col.gray),
+                    labels = c("SigDry" = "Drier", "SigWet" = "Wetter", "NotSig" = "No Change"))  +
   theme(panel.border = element_blank(),
         strip.text = element_text(face = "bold"),
         legend.position = "bottom") +
-  guides(fill = guide_legend(order = 1, title.position = "top", title.hjust = 0.5))
+  guides(fill = guide_legend(order = 1, title.position = "top", title.hjust = 0.5)) +
+  ggsave(file.path("figures_manuscript", "Trends_MannWhitney-Hist.png"),
+         width = 190, height = 95, units = "mm")
 
-# maps
-p_mw_map <- 
+
+# individual maps
+p_mw_afd_map <- 
   ggplot() +
   geom_polygon(data = states, aes(x = long, y = lat, group = group),
                fill = "gray75", color = "white") +
-  geom_point(data = subset(df_mw, mw_p < p_thres),  aes(x = dec_long_va, y = dec_lat_va,
-                                color = mw_diff_mean)) +
-  facet_wrap(~metric, ncol = 1, labeller = 
-               as_labeller(c("annualflowdays" = "(b) Annual Days with Flow",
-                             "peak2z_length" = "(d) Days from Peak to No-Flow",
-                             "zeroflowfirst" = "(f) First No-Flow Day"))) +
+  geom_point(data = subset(df_mw, mw_p < p_thres & metric == "annualnoflowdays"),  
+             aes(x = dec_long_va, y = dec_lat_va, color = mw_diff_mean)) +
+  labs(title = "(d) Annual No-Flow Days") +
   scale_x_continuous(name = "Longitude [\u00B0E]") +
   scale_y_continuous(name = "Latitude [\u00B0N]",
                      breaks = seq(30, 50, 10)) +
-  scale_color_gradient2(name = "Change in Annual Mean\n[(1999 to 2017) - (1980 to 1998)]",
-                        high = "#4575b4", mid = "#ffffbf", low = "#d73027") +
-  #scale_shape_manual(name = "Significance", values = c("TRUE" = 16, "FALSE" = 1),
-  #                   labels = c("TRUE" = "p < 0.05", "FALSE" = "p > 0.05"))  +
+  scale_color_gradient2(name = "Change", 
+                        low = "#4575b4", mid = "#ffffbf", high = "#d73027") +
+  coord_map() +
   theme(panel.border = element_blank(),
         strip.text = element_text(face = "bold"),
+        plot.title = element_text(hjust = 0.5),
+        axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
         legend.position = "bottom") +
-  guides(color = guide_colorbar(order = 1, barwidth = 10,
-                                title.position = "top", title.hjust = 0.5),
-         shape = guide_legend(order = 2, direction = "vertical"))
+  guides(color = guide_colorbar(order = 1, title.position = "top", title.hjust = 0.5, barwidth = 10))
 
-p_mw_combo <- 
-  (p_mw_hist + p_mw_map) +
-  plot_layout(ncol = 2, widths = c(1, 2))
+p_mw_p2z_map <- 
+  ggplot() +
+  geom_polygon(data = states, aes(x = long, y = lat, group = group),
+               fill = "gray75", color = "white") +
+  geom_point(data = subset(df_mw, mw_p < p_thres & metric == "peak2z_length"),  
+             aes(x = dec_long_va, y = dec_lat_va, color = mw_diff_mean)) +
+  labs(title = "(e) Days from Peak to No-Flow") +
+  scale_x_continuous(name = "Longitude [\u00B0E]") +
+  scale_y_continuous(name = "Latitude [\u00B0N]",
+                     breaks = seq(30, 50, 10)) +
+  scale_color_gradient2(name = "Change",
+                        high = "#4575b4", mid = "#ffffbf", low = "#d73027") +
+  coord_map() +
+  theme(panel.border = element_blank(),
+        strip.text = element_text(face = "bold"),
+        plot.title = element_text(hjust = 0.5),
+        axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = "bottom") +
+  guides(color = guide_colorbar(order = 1, title.position = "top", title.hjust = 0.5, barwidth = 10))
 
-ggsave(file.path("figures_Manuscript", "Trends_MannWhitney-Hist+Maps.png"),
-       p_mw_combo, width = 190, height = 220, units = "mm")  
+p_mw_zff_map <- 
+  ggplot() +
+  geom_polygon(data = states, aes(x = long, y = lat, group = group),
+               fill = "gray75", color = "white") +
+  geom_point(data = subset(df_mw, mw_p < p_thres & metric == "zeroflowfirst"),  
+             aes(x = dec_long_va, y = dec_lat_va, color = mw_diff_mean)) +
+  labs(title = "(f) First No-Flow Day") + 
+  scale_x_continuous(name = "Longitude [\u00B0E]") +
+  scale_y_continuous(name = "Latitude [\u00B0N]",
+                     breaks = seq(30, 50, 10)) +
+  scale_color_gradient2(name = "Change",
+                        high = "#4575b4", mid = "#ffffbf", low = "#d73027") +
+  coord_map() +
+  theme(panel.border = element_blank(),
+        strip.text = element_text(face = "bold"),
+        plot.title = element_text(hjust = 0.5),
+        axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = "bottom") +
+  guides(color = guide_colorbar(order = 1, title.position = "top", title.hjust = 0.5, barwidth = 10))
+
+p_mw_map_combo <- 
+  (p_mw_afd_map +
+     p_mw_p2z_map +
+     p_mw_zff_map) + 
+  plot_layout(ncol = 3)
+
+ggsave(file.path("figures_Manuscript", "Trends_MannWhitney-Maps.png"),
+       p_mw_map_combo, width = 190, height = 90, units = "mm")  
+
+
+
+
+
+# individual histograms
+p_mw_afd_hist <-
+  ggplot() +
+  geom_histogram(data = subset(df_mw, metric == "annualnoflowdays"), 
+                 aes(x = mw_diff_mean, fill = mw_sig),
+                 binwidth = 5) +
+  geom_vline(xintercept = 0, color = "black") +
+  labs(title = "(a) Annual No-Flow Days") +
+  scale_x_continuous(name = "Change") +
+  scale_y_continuous(name = "Number of Gages") +
+  scale_fill_manual(name = "Mann-Whitney Significance", guide = NULL,
+                    values = c("SigDry" = col.cat.red, "SigWet" = col.cat.blu, "NotSig" = col.gray),
+                    labels = c("SigDry" = "Drier", "SigWet" = "Wetter", "NotSig" = "No Change"))  +
+  theme(panel.border = element_blank(),
+        strip.text = element_text(face = "bold"),
+        legend.position = "bottom")
+
+p_mw_p2z_hist <-
+  ggplot() +
+  geom_histogram(data = subset(df_mw, metric == "peak2z_length"), 
+                 aes(x = mw_diff_mean, fill = mw_sig),
+                 binwidth = 5) +
+  geom_vline(xintercept = 0, color = "black") +
+  labs(title = "(c) Days from Peak to No-Flow") +
+  scale_x_continuous(name = "Change") +
+  scale_y_continuous(name = "Number of Gages") +
+  scale_fill_manual(name = "Mann-Whitney Significance", guide = NULL,
+                    values = c("SigDry" = col.cat.red, "SigWet" = col.cat.blu, "NotSig" = col.gray),
+                    labels = c("SigDry" = "Drier", "SigWet" = "Wetter", "NotSig" = "No Change"))  +
+  theme(panel.border = element_blank(),
+        strip.text = element_text(face = "bold"),
+        legend.position = "bottom")
+
+p_mw_zff_hist <-
+  ggplot() +
+  geom_histogram(data = subset(df_mw, metric == "zeroflowfirst"), 
+                 aes(x = mw_diff_mean, fill = mw_sig),
+                 binwidth = 5) +
+  geom_vline(xintercept = 0, color = "black") +
+  labs(title = "(e) First No-Flow Day") +
+  scale_x_continuous(name = "Change") +
+  scale_y_continuous(name = "Number of Gages") +
+  scale_fill_manual(name = "Mann-Whitney Significance", guide = NULL,
+                    values = c("SigDry" = col.cat.red, "SigWet" = col.cat.blu, "NotSig" = col.gray),
+                    labels = c("SigDry" = "Drier", "SigWet" = "Wetter", "NotSig" = "No Change"))  +
+  theme(panel.border = element_blank(),
+        strip.text = element_text(face = "bold"),
+        legend.position = "bottom")
