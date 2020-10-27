12c33bc2a5b285bbd39792a7e85fc40359c0c7d9
Sam Zipper <samuelczipper@gmail.com>
2020-10-27 10:55:06 -0500

    Add comparison of Mann-Kendall trends between metrics

--- code/Trends_ExploreResults.R
+++ code/Trends_ExploreResults.R
@@ -6,7 +6,7 @@
 source(file.path("code", "paths+packages.R"))
 
 ## metrics we care about
-metrics <- c("annualflowdays", "zeroflowfirst", "peak2z_length")
+metrics <- c("annualnoflowdays", "zeroflowfirst", "peak2z_length")
 
 ## load data
 gage_regions <- 
@@ -20,6 +20,9 @@ gage_trends <-
   subset(metric %in% metrics) %>% 
   dplyr::left_join(gage_regions, by = "gage_ID") %>% 
   dplyr::left_join(gage_mean[,c("gage_ID", "dec_lat_va", "dec_long_va")], by = "gage_ID")
+
+# threshold for significant
+p_thres <- 0.05
 
 ## plot: mann-kendall results
 # load state map
@@ -36,7 +39,7 @@ ggplot() +
   geom_point(data = df_mk,  aes(x = dec_long_va, y = dec_lat_va,
                                 color = mk_tau, shape = mk_p < p_thres)) +
   facet_wrap(~metric, labeller = 
-               as_labeller(c("annualflowdays" = "(a) Annual Days with Flow",
+               as_labeller(c("annualnoflowdays" = "(a) Annual Days with Flow",
                              "peak2z_length" = "(b) Days from Peak to No-Flow",
                              "zeroflowfirst" = "(c) First No-Flow Day"))) +
   scale_x_continuous(name = "Longitude [\u00B0E]") +
@@ -66,7 +69,7 @@ ggplot() +
                  binwidth = 10) +
   geom_vline(xintercept = 0, color = "black") +
   facet_wrap(~metric, scales = "free", labeller = 
-               as_labeller(c("annualflowdays" = "(a) Annual Days with Flow",
+               as_labeller(c("annualnoflowdays" = "(a) Annual Days with Flow",
                              "peak2z_length" = "(b) Days from Peak to No-Flow",
                              "zeroflowfirst" = "(c) First No-Flow Day"))) +
   scale_x_continuous(name = "Change in Annual Value [1999 to 2017 Mean - 1980 to 1998 Mean]") +
@@ -86,7 +89,7 @@ ggplot() +
   geom_point(data = df_mw,  aes(x = dec_long_va, y = dec_lat_va,
                                 color = mw_diff, shape = mw_p < p_thres)) +
   facet_wrap(~metric, labeller = 
-               as_labeller(c("annualflowdays" = "(a) Annual Days with Flow",
+               as_labeller(c("annualnoflowdays" = "(a) Annual Days with Flow",
                              "peak2z_length" = "(b) Days from Peak to No-Flow",
                              "zeroflowfirst" = "(c) First No-Flow Day"))) +
   scale_x_continuous(name = "Longitude [\u00B0E]") +
@@ -101,62 +104,3 @@ ggplot() +
         legend.position = "bottom") +
   guides(color = guide_colorbar(order = 1, title.position = "top", title.hjust = 0.5),
          shape = guide_legend(order = 2, direction = "vertical"))
-
-
-## summarize
-p_thres <- 0.05
-gage_trends %>% 
-  dplyr::group_by(metric) %>% 
-  dplyr::summarize(n_finite = sum(is.finite(slope)),
-                   n_sig = sum(pval < p_thres, na.rm = T),
-                   n_sig_pos = sum(slope > 0 & pval < p_thres, na.rm = T),
-                   n_sig_neg = sum(slope <= 0 & pval < p_thres, na.rm = T))
-
-207/512
-34/150
-23/123
-
-
-
-## reproducible example - exploring mann-kendal and sen's slope
-# sample data
-df <- 
-  tibble::tibble(year = seq(1980, 2017),
-                 value = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
-                           0, 0, 0, 0, 0, 0, 0, 0, 0, 0.071232877, 0.082191781, 0.008219178, 
-                           0, 0.008219178, 0, 0.273972603, 0.270491803, 0.01369863, 0, 0, 
-                           0.092896175, 0.090410959, 0, 0, 0, 0, 0.024657534))
-
-
-# make sample data with linear increase
-year <- seq(1, 51)
-value <- seq(0, 500, 10)
-
-# replace >50% of values with 0s
-value[seq(1,51,2)] <- 0
-
-# plot
-plot(year, value)
-
-# calculate mann-kendall tau and theil-sen slope
-manken <- rkt::rkt(year, value)
-manken$tau  # Kendall tau = 0.235
-manken$sl   # p-value = 0.009
-manken$B    # Theil-Sen estimator = 0
-
-# test linear slope
-linfit <- lm(value ~ year)
-summary(linfit)
-
-## now, set >50% of values to zero
-df$value_with_0s <- df$value
-df$value_with_0s[seq(1,51,2)] <- 0
-
-# plot
-plot(df$year, df$value_with_0s)
-
-# calculate mann-kendall tau and theil-sen slope
-manken_with_0s <- rkt::rkt(df$year, df$value_with_0s)
-manken_with_0s$tau  # Kendall tau = 0.23
-manken_with_0s$sl   # p-value = 0.009 (significant)
-manken_with_0s$B    # Theil-Sen slope = 0 becuase > 50% of points are the same
--- figures_manuscript/Trends_MannKendall+MannWhitney.R
+++ figures_manuscript/Trends_MannKendall+MannWhitney.R
@@ -16,6 +16,10 @@ gage_trends <-
   subset(metric %in% metrics) %>% 
   dplyr::left_join(gage_mean[,c("gage_ID", "region", "dec_lat_va", "dec_long_va")], by = "gage_ID")
 
+gage_regions <- 
+  readr::read_csv(file.path("results", "00_SelectGagesForAnalysis_GageRegions.csv")) %>% 
+  dplyr::select(gage_ID, region)
+
 # load state map
 states <- map_data("state")
 
@@ -106,40 +110,80 @@ p_zff <-
 
 p_combo <- 
   ((p_afd + ggtitle("(a) Annual No-Flow Days")) + 
-    (p_p2z + ggtitle("(b) Days from Peak to No-Flow")) + 
-    (p_zff + ggtitle("(c) First No-Flow Day"))) +
-  plot_layout(ncol = 1)
+     (p_p2z + ggtitle("(b) Days from Peak to No-Flow")) + 
+     (p_zff + ggtitle("(c) First No-Flow Day"))) +
+  plot_layout(ncol = 1) & 
+  theme(plot.title = element_text(face = "plain"))
 
 ggsave(file.path("figures_manuscript", "Trends_MannKendall-Maps.png"),
        p_combo, width = 190, height = 240, units = "mm")
 
+## comparison among trends
+df_mk_wide <-
+  df_mk %>% 
+  dplyr::select(metric, gage_ID, mk_tau, mk_p) %>% 
+  pivot_wider(names_from = metric, values_from = c(mk_tau, mk_p)) %>% 
+  dplyr::left_join(gage_regions, by = "gage_ID")
 
-# ## single map using facet_wrap
-# p_map <- 
-#   ggplot() +
-#   geom_polygon(data = states, aes(x = long, y = lat, group = group), 
-#                fill = "gray75", color = "white") +
-#   geom_point(data = df_mk,  aes(x = dec_long_va, y = dec_lat_va, 
-#                                 color = mk_tau, shape = mk_p < p_thres)) +
-#   facet_wrap(~metric, ncol = 1, labeller = as_labeller(
-#     c("annualflowdays" = "(a) Annual Days with Flow",
-#       "peak2z_length" = "(b) Days from Peak to No-Flow",
-#       "zeroflowfirst" = "(c) First No-Flow Day"))) +
-#   scale_x_continuous(name = "Longitude [\u00B0E]") +
-#   scale_y_continuous(name = "Latitude [\u00B0N]",
-#                      breaks = seq(30, 50, 10)) +
-#   scale_color_gradient2(name = "Kendall \u03c4", 
-#                         high = "#4575b4", mid = "#ffffbf", low = "#d73027") +
-#   scale_shape_manual(name = "Significance", values = c("TRUE" = 16, "FALSE" = 1),
-#                      labels = c("TRUE" = "p < 0.05", "FALSE" = "p > 0.05"))  +
-#   theme(panel.border = element_blank(),
-#         strip.text = element_text(face = "bold"),
-#         legend.position = "bottom") +
-#   guides(color = guide_colorbar(order = 1, title.position = "top", title.hjust = 0.5),
-#          shape = guide_legend(order = 2, direction = "vertical"))
-# 
-# ggsave(file.path("figures_manuscript", "Trends_MannKendall-Maps.png"),
-#        p_map, width = 95, height = 200, units = "mm")
+p_mk_anf.p2z <- 
+  ggplot(df_mk_wide, aes(x = mk_tau_annualnoflowdays, y = mk_tau_peak2z_length)) +
+  geom_hline(yintercept = 0, color = col.gray) + geom_vline(xintercept = 0, color = col.gray) +
+  geom_point(shape = 1) +
+  scale_x_continuous(name = "Kendall \u03c4, Annual No-Flow Days", 
+                     limits = c(tau_min, tau_max), expand = c(0,0.01)) +
+  scale_y_continuous(name = "Kendall \u03c4, Peak to No-Flow", 
+                     limits = c(tau_min, tau_max), expand = c(0,0.01)) +
+  stat_smooth(method = "lm", color = col.cat.blu)
+
+p_mk_anf.zff <-
+  ggplot(df_mk_wide, aes(x = mk_tau_annualnoflowdays, y = mk_tau_zeroflowfirst)) +
+  geom_hline(yintercept = 0, color = col.gray) + geom_vline(xintercept = 0, color = col.gray) +
+  geom_point(shape = 1) +
+  scale_x_continuous(name = "Kendall \u03c4, Annual No-Flow Days", 
+                     limits = c(tau_min, tau_max), expand = c(0,0.01)) +
+  scale_y_continuous(name = "Kendall \u03c4, First No-Flow Day", 
+                     limits = c(tau_min, tau_max), expand = c(0,0.01)) +
+  stat_smooth(method = "lm", color = col.cat.blu)
+
+p_mk_p2z.zff <-
+  ggplot(df_mk_wide, aes(y = mk_tau_peak2z_length, x = mk_tau_zeroflowfirst)) +
+  geom_hline(yintercept = 0, color = col.gray) + geom_vline(xintercept = 0, color = col.gray) +
+  geom_point(shape = 1) +
+  scale_y_continuous(name = "Kendall \u03c4, Peak to No-Flow", 
+                     limits = c(tau_min, tau_max), expand = c(0,0.01)) +
+  scale_x_continuous(name = "Kendall \u03c4, First No-Flow Day", 
+                     limits = c(tau_min, tau_max), expand = c(0,0.01)) +
+  stat_smooth(method = "lm", color = col.cat.blu)
+
+((p_mk_anf.p2z + p_mk_anf.zff + p_mk_p2z.zff) +
+    plot_layout(ncol = 1) + 
+    plot_annotation(tag_levels = 'a', tag_prefix = '(', tag_suffix = ')') & 
+    theme(plot.title = element_text(face = "plain"),
+          plot.tag.position = c(0.07, 1))) +
+  ggsave(file.path("figures_manuscript", "Trends_MannKendall-MetricComparison.png"),
+         width = 95, height = 210, units = "mm")
+
+## mann-kendall stats
+# percent of gages with significant, positive, negative trends
+df_mk %>% subset(metric == "annualnoflowdays" & mk_p < p_thres) %>% dim()/540
+df_mk %>% subset(metric == "annualnoflowdays" & mk_tau > 0 & mk_p < p_thres) %>% dim()/540 # drying
+df_mk %>% subset(metric == "annualnoflowdays" & mk_tau < 0 & mk_p < p_thres) %>% dim()/540 # wetting
+df_mk %>% subset(metric == "annualnoflowdays") %>% dim()/540 # sufficient data
+
+df_mk %>% subset(metric == "peak2z_length" & mk_p < p_thres) %>% dim()/540
+df_mk %>% subset(metric == "peak2z_length" & mk_tau < 0 & mk_p < p_thres) %>% dim()/540 # faster/drying
+df_mk %>% subset(metric == "peak2z_length" & mk_tau > 0 & mk_p < p_thres) %>% dim()/540 # slower/wetting
+df_mk %>% subset(metric == "peak2z_length") %>% dim()/540 # sufficient data
+
+df_mk %>% subset(metric == "zeroflowfirst" & mk_p < p_thres) %>% dim()/540
+df_mk %>% subset(metric == "zeroflowfirst" & mk_tau < 0 & mk_p < p_thres) %>% dim()/540 # earlier/drying
+df_mk %>% subset(metric == "zeroflowfirst" & mk_tau > 0 & mk_p < p_thres) %>% dim()/540 # later/wetting
+df_mk %>% subset(metric == "zeroflowfirst") %>% dim()/540 # sufficient data
+
+# correlation between trends
+cor(x = df_mk_wide$mk_tau_annualnoflowdays, y = df_mk_wide$mk_tau_peak2z_length, use = "complete.obs")
+cor(x = df_mk_wide$mk_tau_annualnoflowdays, y = df_mk_wide$mk_tau_zeroflowfirst, use = "complete.obs")
+cor(x = df_mk_wide$mk_tau_zeroflowfirst, y = df_mk_wide$mk_tau_peak2z_length, use = "complete.obs")
 
 ####
 #### mann-whitney
@@ -185,7 +229,6 @@ p_mw_hist <-
                     values = c("SigDry" = col.cat.red, "SigWet" = col.cat.blu, "NotSig" = col.gray),
                     labels = c("SigDry" = "Drier", "SigWet" = "Wetter", "NotSig" = "No Change"))  +
   theme(panel.border = element_blank(),
-        strip.text = element_text(face = "bold"),
         legend.position = "bottom") +
   guides(fill = guide_legend(order = 1, title.position = "top", title.hjust = 0.5)) +
   ggsave(file.path("figures_manuscript", "Trends_MannWhitney-Hist.png"),
@@ -207,8 +250,7 @@ p_mw_afd_map <-
                         low = "#4575b4", mid = "#ffffbf", high = "#d73027") +
   coord_map() +
   theme(panel.border = element_blank(),
-        strip.text = element_text(face = "bold"),
-        plot.title = element_text(hjust = 0.5),
+        plot.title = element_text(hjust = 0.5, face = "plain"),
         axis.title = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank(),
@@ -229,8 +271,7 @@ p_mw_p2z_map <-
                         high = "#4575b4", mid = "#ffffbf", low = "#d73027") +
   coord_map() +
   theme(panel.border = element_blank(),
-        strip.text = element_text(face = "bold"),
-        plot.title = element_text(hjust = 0.5),
+        plot.title = element_text(hjust = 0.5, face = "plain"),
         axis.title = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank(),
@@ -251,8 +292,7 @@ p_mw_zff_map <-
                         high = "#4575b4", mid = "#ffffbf", low = "#d73027") +
   coord_map() +
   theme(panel.border = element_blank(),
-        strip.text = element_text(face = "bold"),
-        plot.title = element_text(hjust = 0.5),
+        plot.title = element_text(hjust = 0.5, face = "plain"),
         axis.title = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank(),
@@ -267,56 +307,3 @@ p_mw_map_combo <-
 
 ggsave(file.path("figures_Manuscript", "Trends_MannWhitney-Maps.png"),
        p_mw_map_combo, width = 190, height = 90, units = "mm")  
-
-
-
-
-
-# individual histograms
-p_mw_afd_hist <-
-  ggplot() +
-  geom_histogram(data = subset(df_mw, metric == "annualnoflowdays"), 
-                 aes(x = mw_diff_mean, fill = mw_sig),
-                 binwidth = 5) +
-  geom_vline(xintercept = 0, color = "black") +
-  labs(title = "(a) Annual No-Flow Days") +
-  scale_x_continuous(name = "Change") +
-  scale_y_continuous(name = "Number of Gages") +
-  scale_fill_manual(name = "Mann-Whitney Significance", guide = NULL,
-                    values = c("SigDry" = col.cat.red, "SigWet" = col.cat.blu, "NotSig" = col.gray),
-                    labels = c("SigDry" = "Drier", "SigWet" = "Wetter", "NotSig" = "No Change"))  +
-  theme(panel.border = element_blank(),
-        strip.text = element_text(face = "bold"),
-        legend.position = "bottom")
-
-p_mw_p2z_hist <-
-  ggplot() +
-  geom_histogram(data = subset(df_mw, metric == "peak2z_length"), 
-                 aes(x = mw_diff_mean, fill = mw_sig),
-                 binwidth = 5) +
-  geom_vline(xintercept = 0, color = "black") +
-  labs(title = "(c) Days from Peak to No-Flow") +
-  scale_x_continuous(name = "Change") +
-  scale_y_continuous(name = "Number of Gages") +
-  scale_fill_manual(name = "Mann-Whitney Significance", guide = NULL,
-                    values = c("SigDry" = col.cat.red, "SigWet" = col.cat.blu, "NotSig" = col.gray),
-                    labels = c("SigDry" = "Drier", "SigWet" = "Wetter", "NotSig" = "No Change"))  +
-  theme(panel.border = element_blank(),
-        strip.text = element_text(face = "bold"),
-        legend.position = "bottom")
-
-p_mw_zff_hist <-
-  ggplot() +
-  geom_histogram(data = subset(df_mw, metric == "zeroflowfirst"), 
-                 aes(x = mw_diff_mean, fill = mw_sig),
-                 binwidth = 5) +
-  geom_vline(xintercept = 0, color = "black") +
-  labs(title = "(e) First No-Flow Day") +
-  scale_x_continuous(name = "Change") +
-  scale_y_continuous(name = "Number of Gages") +
-  scale_fill_manual(name = "Mann-Whitney Significance", guide = NULL,
-                    values = c("SigDry" = col.cat.red, "SigWet" = col.cat.blu, "NotSig" = col.gray),
-                    labels = c("SigDry" = "Drier", "SigWet" = "Wetter", "NotSig" = "No Change"))  +
-  theme(panel.border = element_blank(),
-        strip.text = element_text(face = "bold"),
-        legend.position = "bottom")
