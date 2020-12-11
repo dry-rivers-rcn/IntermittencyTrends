bd4d0ba7c4068fb0c82ef3ac32717af55b36361f
Sam Zipper <samuelczipper@gmail.com>
2020-12-11 08:52:38 -0600

    Replace regional trend boxplots with violin plots

--- figures_manuscript/Trends_Violins.R
+++ figures_manuscript/Trends_Violins.R
@@ -1,4 +1,4 @@
-## Trends_Boxplots.R
+## Trends_Violins.R
 #' This script load the trend results and makes regional boxplots.
 #' It is modified from Kendra's code in 'code/TrendPatterns.R'
 #' 
@@ -43,7 +43,7 @@ p_region <-
   gage_hydro_trends %>% 
   ggplot(aes(x = region, y = mk_tau, fill = region, color = region)) +
   geom_hline(yintercept = 0, color = col.gray) +
-  geom_boxplot(outlier.shape = 1) +
+  geom_violin(draw_quantiles = 0.5) +
   facet_wrap(~metric, ncol = 1, dir = "v",
              scales = "free_y", labeller = as_labeller(labs_metrics)) +
   scale_x_discrete(name = "Region", labels = lab_regions_skinny) +
@@ -51,7 +51,7 @@ p_region <-
                      breaks = seq(-0.6, 0.6, 0.3), expand = c(0, 0.01)) +
   scale_fill_manual(values = pal_regions, guide = NULL) +
   scale_color_manual(values = pal_regions_dk, guide = NULL) +
-  ggsave(file.path("figures_manuscript", "Trends_Boxplots-Region.png"),
+  ggsave(file.path("figures_manuscript", "Trends_Violins-Region.png"),
          width = 95, height = 240, units = "mm")
 
 ## boxplot by latitude
@@ -62,7 +62,7 @@ gage_hydro_trends$lat_bin <- cut(gage_hydro_trends$dec_lat_va,
 p_lat <- 
   ggplot(gage_hydro_trends, aes(x = lat_bin, y = mk_tau)) +
   geom_hline(yintercept = 0, color = col.gray) +
-  geom_boxplot(outlier.shape = 1) +
+  geom_violin(draw_quantiles = 0.5) +
   facet_wrap(~metric, ncol = 1, dir = "v",
              scales = "free_y", labeller = as_labeller(labs_metrics)) +
   coord_flip() +
@@ -72,6 +72,12 @@ p_lat <-
 
 
 # combine    
-(p_region + p_lat) + 
-  ggsave(file.path("figures_manuscript", "Trends_Boxplots-Region+Lat_NoText.pdf"),
+(p_region + p_lat + plot_layout(widths = c(0.6, 0.4))) + 
+  ggsave(file.path("figures_manuscript", "Trends_Violins-Region+Lat_NoText.pdf"),
          width = 190, height = 140, units = "mm", device = cairo_pdf)
+
+# number by region
+gage_hydro_trends %>% 
+  subset(is.finite(mk_tau)) %>% 
+  dplyr::group_by(metric, region) %>% 
+  dplyr::summarize(n_gages = n())
